54c429986ac471e4879243efd63a62cbe7486818
Papamani <30549690+Papamani@users.noreply.github.com>
2017-08-08 16:02:26 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enter the number");
+scanf("%d",&n);
+if(n/2==0)
+{
+printf("The given number is even");
+}
+else
+printf("The given number is odd");
+getch();
+}
