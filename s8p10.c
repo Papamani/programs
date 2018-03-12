c746b0b0302ad826bf09a8cc216e0248c404e7ae
Papamani <30549690+Papamani@users.noreply.github.com>
2018-03-12 16:14:13 +0530

    s8pg10.c

---
+++ s8p10.c
@@ -0,0 +1,17 @@
+#include<studio.h>
+int main()
+{
+int n,a[10],n1;
+printf("Enter the numbers:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]))
+}
+n1=a[i]%10;
+if(n1%2!=0)
+{
+printf("%d",n1);
+}
+return 0;
+}
