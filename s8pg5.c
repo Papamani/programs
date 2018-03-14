440b78dedc2b273b971978c04bce1c89e23d667e
Papamani <30549690+Papamani@users.noreply.github.com>
2018-03-14 15:50:00 +0530

    seprate string

---
+++ s8pg5.c
@@ -0,0 +1,26 @@
+#include<studio.h>
+#include<string.h>
+int main()
+{
+int i,n;
+char m[10];
+printf("Enter the character:");
+scanf("%s",m);
+n=strlen(m);
+for(i=0;i<n;i++)
+{
+if(i%2==0)
+{
+printf("%c",a[i]);
+}
+}
+printf("\t");
+for(i=0;i<n;i++);
+{
+if(i%2!=0)
+{
+printf("%c),a[i]);
+}
+}
+return o;
+}
