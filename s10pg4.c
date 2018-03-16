967705175b129ac240526a15685d6f806acca3fb
Papamani <30549690+Papamani@users.noreply.github.com>
2018-03-16 15:57:37 +0530

    follow

--- s10pg4.c
+++ s10pg4.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int n[10],k,i;
+int n,a[10],k,i;
 printf("Enter the two numbers:");
 scanf("%d%d",&n,&k);
 for(i=0;i<n;i++)
 {
-scanf("%d",n[i]);
+scanf("%d",a[i]);
 }
 printf("The follow of n is: %d",k);
 return 0;
